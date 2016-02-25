34f968cbd654cd56c71cf3228d7b931cfe718716
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-02-25 22:34:34 +0900

    リサイズから9分割まで

--- PhotoMaster/Image.swift
+++ PhotoMaster/Image.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import UIKit
+
+class Image : UIImage {
+    var image : UIImage!
+    
+    func resize(var image:UIImage)->UIImage{
+        if image.size.width != image.size.height{
+            let length = min(image.size.height,image.size.width)
+            //リサイズ
+            let size = CGSize(width: length, height: length)
+            UIGraphicsBeginImageContext(size)
+            image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+            image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+        return image
+    }
+    
+    func cutInto9Pieces(image:UIImage)->[UIImage]{
+        var imageArray : [UIImage] = []
+        // イメージの解像度に従いrectも換算
+        //let scale = image.scale
+        //一辺の長さ
+        let length = image.size.width/3
+        for var i=0;i<3;i++ {
+            for var j=0;j<3;j++ {
+                let cropRect : CGRect = CGRectMake(length*CGFloat(i),length*CGFloat(j),length,length)
+                // 指定された範囲を切り抜いたCGImageRefを生成しUIImageとする
+                
+                let cropRef   = CGImageCreateWithImageInRect(image.CGImage, cropRect)
+                let cropImage = UIImage(CGImage: cropRef!)
+                
+                imageArray.append(cropImage)
+            }
+        }
+        
+        return imageArray
+    }
+       
+    
+    
+}
--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,11 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+    @IBOutlet var photoImageView : UIImageView!
+    var selectedImage = Image()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func precentPickerController(sourceType:UIImagePickerControllerSourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.presentViewController(picker, animated:true, completion:nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: NSDictionary!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        selectedImage.image = image
+        photoImageView.image = selectedImage.image
+    }
+    
+    @IBAction func selectButtonTapped(sender: UIButton){
+        //選択肢の上に表示するアラート
+        let alert = UIAlertController(title: "画像の取得先を選択",message: nil, preferredStyle: .ActionSheet)
+        //選択肢設定
+        let firstAction = UIAlertAction(title: "カメラ", style: .Default){
+            action in
+            self.precentPickerController(.Camera)
+        }
+        let secondAction = UIAlertAction(title: "アルバム", style: .Default){
+            action in
+            self.precentPickerController(.PhotoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .Default,handler : nil)
+        
+        //選択肢をアラートに登録
+        alert.addAction(firstAction)
+        alert.addAction(secondAction)
+        alert.addAction(cancelAction)
+        
+        //アラートを表示
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func resize(){
+        selectedImage.image =  selectedImage.resize(selectedImage.image)
+        photoImageView.image = selectedImage.image
+        
+    }
+    
+    @IBAction func cutInto9Pieces(){
+        var imageArray = selectedImage.cutInto9Pieces(selectedImage.image)
+        photoImageView.image = imageArray[0]
     }
 
     override func didReceiveMemoryWarning() {
